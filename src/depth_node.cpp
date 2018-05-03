b967bf0428b1ea2ddc4b69219c9fffc4dc2ea7ab
Jacqueline Deans <jdeans289@gmail.com>
2018-05-03 15:44:09 -0500

    updated depth_node to visualize a cube

--- src/depth_node.cpp
+++ src/depth_node.cpp
@@ -3,10 +3,12 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <cv.h>
-#include <geometry_msgs/Pose.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <vector>
+#include <frispy/TFBroadcastPR.h>
+#include <frispy/mapperPR.h>
+#include <visualization_msgs/Marker.h>
 
 
 
@@ -44,12 +46,74 @@ void getDepth(const sensor_msgs::ImageConstPtr& msg) //int x, int y)
 int main (int argc, char** argv) {
 	ros::init(argc, argv, "depth");
 	ros::NodeHandle node;
+    ros::Rate r(1);
 
 	ros::Subscriber depth_subscriber = node.subscribe("/nav_kinect/depth_registered/image_raw"
 													, 100, getDepth);//boost::bind(getDepth, _1, argv[0], argv[1]));
-	for (int i = 0; i < 10; i++) {
-		ROS_INFO("%d", i);
-	}
-	ros::spin();
+	
+    ros::Publisher marker_pub = node.advertise<visualization_msgs::Marker>("detected_object", 1);
+    uint32_t shape = visualization_msgs::Marker::CUBE;
+
+    
+
+
+    while (ros::ok()) {
+
+        visualization_msgs::Marker marker;
+
+        marker.header.frame_id = "nav_kinect_rgb_optical_frame";
+        marker.header.stamp = ros::Time::now();
+      
+         // Set the namespace and id for this marker.  This serves to create a unique ID
+             // Any marker sent with the same namespace and id will overwrite the old one
+        marker.ns = "basic_shapes";
+        marker.id = 0;
+        marker.type = shape;
+        marker.action = visualization_msgs::Marker::ADD;
+
+
+        // TFBroadcastPR broadcaster("odom","camera_rgb_optical_frame");
+        // mapperPR mapper(broadcaster);
+
+        geometry_msgs::Pose pose;
+        pose.position.x = 1;
+        pose.position.y = 1;
+        pose.position.z = 1;
+
+        pose.orientation.x = 0;
+        pose.orientation.y = 0;
+        pose.orientation.z = 0;
+        pose.orientation.w = 1;
+
+
+        // set scale
+        marker.scale.x = 1.0;
+        marker.scale.y = 1.0;
+        marker.scale.z = 1.0;
+
+        // set color
+        marker.color.r = 0.0f;
+        marker.color.g = 1.0f;
+        marker.color.b = 0.0f;
+        marker.color.a = 1.0;
+        
+        marker.pose = pose;
+        marker.lifetime = ros::Duration();
+        marker_pub.publish(marker);
+
+        //ros::spinOnce();  // calling spinOnce here prevents the cube from being published!
+
+        r.sleep();
+
+    }
+
+    // mapper.receivePose(pose);
+
+
+
+ //    for (int i = 0; i < 10; i++) {
+	// 	ROS_INFO("%d", i);
+	// }
+	
   	return 0;
 }
--- src/mapperPR.cpp
+++ src/mapperPR.cpp
@@ -5,3 +5,4 @@ mapperPR::mapperPR(TFBroadcastPR& br) : _broadcaster(br) {}
 void mapperPR::receivePose(geometry_msgs::Pose &pose) {
 	_broadcaster.receivePose(pose);
 }
+
